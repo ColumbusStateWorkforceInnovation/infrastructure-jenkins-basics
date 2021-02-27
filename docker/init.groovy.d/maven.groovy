14f3249f06360527c20e081ee9874379c24abe74
Luke Rouker <lrouker@WFIL061.cscc.local>
2021-02-27 15:45:20 -0500

    Removed stuff that will go in lab 10

--- docker/init.groovy.d/maven.groovy
+++ docker/init.groovy.d/maven.groovy
@@ -1,21 +1,23 @@
-import hudson.tasks.Maven
-import hudson.tasks.Maven.MavenInstallation;
-import hudson.tools.InstallSourceProperty;
-import hudson.tools.ToolProperty;
-import hudson.tools.ToolPropertyDescriptor
-import hudson.tools.ZipExtractionInstaller;
-import hudson.util.DescribableList
-import jenkins.model.Jenkins;
+import hudson.model.JDK
+import hudson.tools.InstallSourceProperty
+import hudson.tools.ZipExtractionInstaller
 
-/* This runs at startup (since it's in init.groovy.d).  It installs maven */
+def descriptor = new JDK.DescriptorImpl();
 
-def extensions = Jenkins.instance.getExtensionList(Maven.DescriptorImpl.class)[0]
-List<MavenInstallation> installations = []
-mavenToool = ['name': 'maven3', 'url': 'file:/usr/share/jenkins/ref/apache-maven-3.6.3-bin.tar.gz', 'subdir': 'apache-maven-3.6.3']
-println("Setting up tool: ${mavenToool.name} ")
-def describableList = new DescribableList<ToolProperty<?>, ToolPropertyDescriptor>()
-def installer = new ZipExtractionInstaller(mavenToool.label as String, mavenToool.url as String, mavenToool.subdir as String);
-describableList.add(new InstallSourceProperty([installer]))
-installations.add(new MavenInstallation(mavenToool.name as String, "", describableList))
-extensions.setInstallations(installations.toArray(new MavenInstallation[installations.size()]))
-extensions.save()
+
+def List<JDK> installations = []
+
+javaTools=[['name':'jdk8', 'url':'file:/var/jenkins_home/downloads/jdk-8u144-linux-x64.tar.gz', 'subdir':'jdk1.8.0_144'],
+      ['name':'jdk7', 'url':'file:/var/jenkins_home/downloads/jdk-7u80-linux-x64.tar.gz', 'subdir':'jdk1.7.0_80']]
+
+javaTools.each { javaTool ->
+
+    println("Setting up tool: ${javaTool.name}")
+
+    def installer = new ZipExtractionInstaller(javaTool.label as String, javaTool.url as String, javaTool.subdir as String);
+    def jdk = new JDK(javaTool.name as String, null, [new InstallSourceProperty([installer])])
+    installations.add(jdk)
+
+}
+descriptor.setInstallations(installations.toArray(new JDK[installations.size()]))
+descriptor.save()
